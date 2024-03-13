da07af365914642d8ed7fb146b28f4b7f0918f08
akhilsurnedi5479 <akhilsurnedi5479@gmail.com>
2024-03-13 15:03:16 -0400

    Changes Added to portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
+import { PLATFORM_ID } from '@angular/core';
 
+import AOS from 'aos'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Portfolio';
+  ngOnInit(){
+    
+  }
+  
+
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object){
+    if (isPlatformBrowser(this.platformId)) { AOS.init({ once: true, duration: 1000 }); }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {NgxTypedJsModule} from 'ngx-typed-js';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { ResumeComponent } from './resume/resume.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    ResumeComponent,
+    AboutComponent,
+    HomeComponent,
+    PortfolioComponent,
+    ContactComponent,
+    FooterComponent,
+    HeroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxTypedJsModule
   ],
   providers: [
     provideClientHydration()

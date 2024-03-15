19150eb32846ebb273e8b762e83edfd3778a2a20
akhilsurnedi5479 <akhilsurnedi5479@gmail.com>
2024-03-15 09:45:06 -0400

    modified ux

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
+import { fromEvent, throttleTime } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  enableSticky= false;
+  enableTab= true;
+  constructor(@Inject(DOCUMENT)private document:Document){
+
+  }
+  ngOnInit(): void {
+    fromEvent(window,'scroll').pipe(throttleTime(100)).subscribe((event)=>{
+      if(this.document.defaultView.scrollY >50){
+        this.enableSticky = true;
+        this.enableTab=false;
+        console.log("position ",this.document.defaultView.scrollY);
+      }
+      
+      else{
+        this.enableSticky=false;
+        this.enableTab=true;
+        console.log("position ",this.document.defaultView.scrollY);
+      }
+      
+      
+    })
+    
+  }
 
 }
